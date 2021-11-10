cf8876a085fb8e71fb49ad60625f2b2fe254189c
aldoortiz128 <aldoortiz128@gmail.com>
2021-11-10 15:56:04 +0000

    accomplished

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-//write your code here
+//let new string = arg.
+//let newstring = '' -- Use this string to build solution.
+//For loop to iterate throughout the string.
+//Check if the char is a lowercase letter or special character.
+//Add the character to the end of new string.
+//Return new string.
+
+function stripUpperCase(string) {
+    
+ let newstring='';
+ 
+ for (let i = 0; i < string.length; i++) {
+ if (string[i] === string[i].toLowerCase()) {newstring+=string[i]}
+} 
+
+return newstring;
+}
+
+
+console.log(stripUpperCase('Papa!%'));
